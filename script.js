cdc784ee9fca3b35ab35b7e64af0c475c0c39604
tugabuy <tugabuy@protonmail.com>
2025-03-13 02:22:14 +0000

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
+    const menuToggle = document.getElementById("menu-toggle");
     const sidebar = document.querySelector(".sidebar");
 
     menuToggle.addEventListener("click", function() {
