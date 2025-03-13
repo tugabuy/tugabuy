dd6c82ad2c33d8d8bdd81ec34307229fa024d6e1
tugabuy <tugabuy@protonmail.com>
2025-03-13 17:25:28 +0000

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const fixedLogo = document.getElementById("fixedLogo");
     const menuToggle = document.getElementById("menuToggle");
     const sidebar = document.getElementById("sidebar");
 
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            fixedLogo.classList.add("hidden");
+        } else {
+            fixedLogo.classList.remove("hidden");
+        }
+    });
+
     menuToggle.addEventListener("click", function () {
-        sidebar.classList.toggle("active");
+        sidebar.classList.toggle("show");
     });
 });
