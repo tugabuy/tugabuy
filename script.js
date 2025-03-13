4d3781e0419c44bfe3f049b5ac34dc2d22b877b0
tugabuy <tugabuy@protonmail.com>
2025-03-13 05:29:45 +0000

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".menu-icon");
-    const menu = document.querySelector(".menu");
-
-    menuIcon.addEventListener("click", function () {
-        menu.classList.toggle("open");
-    });
-});
+function toggleMenu() {
+    document.querySelector('.menu').classList.toggle('open');
+}
